e7283f08d2b76c9ef8b18f34234f6a418b52c1f0
Arul Saxena <saxena@cs.ucsb.edu>
2022-03-03 13:13:39 -0800

    Add 404 page

--- src/NoPage.js
+++ src/NoPage.js
@@ -1,7 +1,14 @@
+import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const NoPage = () => {
-  return <h1>404</h1>;
-};
+function NoPage() {
+  return (
+    <div>
+      <Container fluid="xxl" className="py-3">
+        <h1>Page not found :/</h1>
+      </Container>
+    </div>
+  );
+}
 
 export default NoPage;
