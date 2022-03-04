2beeb5a36d7018afffa84ac9dee6a7523e870f2e
Arul Saxena <saxena@cs.ucsb.edu>
2022-03-04 01:09:37 -0800

    Update about

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,23 @@ function About() {
   return (
     <div>
       <Container fluid="xxl" className="py-3">
-        <h1>About</h1>
-        <p>Lorem ipsum dolor sit amet, consectetaur adipisicing elit, s</p>
+        <h1>What is a spoonerism?</h1>
+        <p>
+          A spoonerism is an error in speech where letters from the beginnings
+          of two words are swapped. For example, <b>(j)elly (b)eans</b> becomes{" "}
+          <b>(b)elly (j)eans</b> as the first letter of each word is substituted
+          for the other.
+        </p>
+        <p>
+          Spoonerisms can involve <i>multigraph swaps</i> as well, where letter
+          groups are swapped, e.g. <b>(cr)ushing (bl)ow</b> to{" "}
+          <b>(bl)ushing (cr)ow</b>.
+        </p>
+        <p>
+          Spelling can be adjusted as necessary—<b>(l)oving (sh)epherd</b> to{" "}
+          <b>(sh)oving (l)eopard</b>—as can pronunciation—<b>(h)ead (b)and</b>{" "}
+          to <b>(b)ead (h)and</b>.
+        </p>
       </Container>
     </div>
   );
